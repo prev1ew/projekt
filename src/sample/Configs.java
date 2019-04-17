8ad8a852918c511b177dd14b95d5bd23d3098b9f
prev1ew <49633811+prev1ew@users.noreply.github.com>
2019-04-18 00:43:23 +0200

    Delete Configs.java

--- src/sample/Configs.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-public class Configs {
-    protected String dbHost = "localhost";
-    protected String dbPort = "3306";
-    protected String dbUser = "root";
-    protected String dbPass = "12345";
-    protected String dbName = "projekt";
-}
